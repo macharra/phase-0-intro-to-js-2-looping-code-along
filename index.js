72ef8e8ab4ba05563ebc46e878060c8eb4d30ca0
rogers.machara <rogersmachara13@gmail.com>
2024-03-27 15:10:55 +0300

    using loops

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names) {
+    let array = [];
+    for (let i = 0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return array;
+}
+
+function countDown(number) {
+        while (number > 0) {
+          console.log(number);
+          number -= 1;
+        }
+        console.log(number);
+    
+}
